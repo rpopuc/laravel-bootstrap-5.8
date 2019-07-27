fa4a4cb53035cdf7d02b94aebd43cfa99393d816
Robson P Oliveira <rpopuc@gmail.com>
2019-07-27 16:46:49 -0300

    Traduz páginas de registro/autenticação de usuário

---
+++ src/resources/lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'A senha deve ter pelo menos oito caracteres e coincidir com a confirmação.',
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos um email com o link para redefinição da senha!',
+    'token' => 'Este código de redefinição de senha é inválido.',
+    'user' => "Não conseguimos encontrar um usuário com esse email.",
+
+];
